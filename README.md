84925e55376d6fcfb3e5378a3e711acd2feedf67
sanzoid <15680086+sanzoid@users.noreply.github.com>
2020-09-23 16:01:03 -0400

    initial project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SZDCommons
 # SZDFavourites
---
+++ SZDFavourites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SZDFavourites
+//
+//  Created by Sandy House on 2020-09-23.
+//  Copyright © 2020 sandzapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
